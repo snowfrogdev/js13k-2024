06034e8b74940d42e8ba899292fcc812a9cba013
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2024-08-25 01:56:39 -0400

    Implement player and projectiles

--- src/main.ts
+++ src/main.ts
@@ -1,161 +1,94 @@
-// import module
-import * as LittleJS from "littlejsengine";
-const { tile, vec2, hsl } = LittleJS;
+import {
+  Color,
+  drawRect,
+  drawTile,
+  engineInit,
+  EngineObject,
+  keyIsDown,
+  mouseIsDown,
+  mousePos,
+  rgb,
+  tile,
+  vec2,
+  Vector2,
+} from "littlejsengine";
 
-// show the LittleJS splash screen
-LittleJS.setShowSplashScreen(true);
-
-// sound effects
-const sound_click = new LittleJS.Sound([1, 0.5]);
-
-// medals
-const medal_example = new LittleJS.Medal(
-  0,
-  "Example Medal",
-  "Welcome to LittleJS!"
-);
-LittleJS.medalsInit("Hello World");
-
-// game variables
-let particleEmitter: {
-  elasticity: number;
-  trailScale: number;
-  colorStartA: { scale: (arg0: number, arg1: number) => any };
-  colorStartB: { scale: (arg0: number, arg1: number) => any };
-  colorEndA: any;
-  colorEndB: any;
-  pos: any;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-function gameInit() {
-  // create tile collision and visible tile layer
-  const tileCollisionSize = vec2(32, 16);
-  LittleJS.initTileCollision(tileCollisionSize);
-  const pos = vec2();
-  const tileLayer = new LittleJS.TileLayer(pos, tileCollisionSize);
-
-  // get level data from the tiles image
-  const mainContext = LittleJS.mainContext;
-  const tileImage = LittleJS.textureInfos[0].image;
-  mainContext.drawImage(tileImage, 0, 0);
-  const imageData = mainContext.getImageData(
-    0,
-    0,
-    tileImage.width,
-    tileImage.height
-  ).data;
-  for (pos.x = tileCollisionSize.x; pos.x--; )
-    for (pos.y = tileCollisionSize.y; pos.y--; ) {
-      // check if this pixel is set
-      const i = pos.x + tileImage.width * (15 + tileCollisionSize.y - pos.y);
-      if (!imageData[4 * i]) continue;
-
-      // set tile data
-      const tileIndex = 1;
-      const direction = LittleJS.randInt(4);
-      const mirror = !LittleJS.randInt(2);
-      const color = LittleJS.randColor();
-      const data = new LittleJS.TileLayerData(
-        tileIndex,
-        direction,
-        mirror,
-        color
-      );
-      tileLayer.setData(pos, data);
-      LittleJS.setTileCollisionData(pos, 1);
-    }
-
-  // draw tile layer with new data
-  tileLayer.redraw();
-
-  // move camera to center of collision
-  LittleJS.setCameraPos(tileCollisionSize.scale(0.5));
-  LittleJS.setCameraScale(48);
-
-  // enable gravity
-  LittleJS.setGravity(-0.01);
-
-  // create particle emitter
-  particleEmitter = new LittleJS.ParticleEmitter(
-    vec2(16, 9),
-    0, // emitPos, emitAngle
-    1,
-    0,
-    500,
-    Math.PI, // emitSize, emitTime, emitRate, emiteCone
-    tile(0, 16), // tileIndex, tileSize
-    hsl(1, 1, 1),
-    hsl(0, 0, 0), // colorStartA, colorStartB
-    hsl(0, 0, 0, 0),
-    hsl(0, 0, 0, 0), // colorEndA, colorEndB
-    2,
-    0.2,
-    0.2,
-    0.1,
-    0.05, // time, sizeStart, sizeEnd, speed, angleSpeed
-    0.99,
-    1,
-    1,
-    Math.PI, // damping, angleDamping, gravityScale, cone
-    0.05,
-    0.5,
-    true,
-    true // fadeRate, randomness, collide, additive
-  );
-  particleEmitter.elasticity = 0.3; // bounce when it collides
-  particleEmitter.trailScale = 2; // stretch in direction of motion
-}
-
-///////////////////////////////////////////////////////////////////////////////
-function gameUpdate() {
-  if (LittleJS.mouseWasPressed(0)) {
-    // play sound when mouse is pressed
-    sound_click.play(LittleJS.mousePos);
-
-    // change particle color and set to fade out
-    particleEmitter.colorStartA = hsl();
-    particleEmitter.colorStartB = LittleJS.randColor();
-    particleEmitter.colorEndA = particleEmitter.colorStartA.scale(1, 0);
-    particleEmitter.colorEndB = particleEmitter.colorStartB.scale(1, 0);
-
-    // unlock medals
-    medal_example.unlock();
+class Player extends EngineObject {
+  constructor() {
+    super();
   }
 
-  // move particles to mouse location if on screen
-  if (LittleJS.mousePosScreen.x) particleEmitter.pos = LittleJS.mousePos;
+  update() {
+    // wasd input for movement
+    const speed = 0.17;
+    if (keyIsDown("KeyW")) this.pos.y += speed;
+    if (keyIsDown("KeyS")) this.pos.y -= speed;
+    if (keyIsDown("KeyA")) this.pos.x -= speed;
+    if (keyIsDown("KeyD")) this.pos.x += speed;
+  }
+
+  render() {
+    drawTile(this.pos, vec2(1), tile(0, 16));
+  }
 }
 
-///////////////////////////////////////////////////////////////////////////////
-function gameUpdatePost() {}
+let player: Player;
 
-///////////////////////////////////////////////////////////////////////////////
+class Projectile extends EngineObject {
+  /**
+   * Constructs a new instance of the Projectile class.
+   * @param position - The position of the instance.
+   * @param direction - The direction of the instance. Must be a unit vector.
+   */
+  constructor(position: Vector2, private direction: Vector2) {
+    super(position);
+    const speed = 0.5;
+    this.velocity = this.direction.scale(speed);
+  }
+
+  render() {
+    drawRect(this.pos, vec2(0.2), rgb(255, 255, 0));
+  }
+}
+
+function gameInit() {
+  player = new Player();
+}
+
+let lastFireTime = 0;
+
+function gameUpdate() {
+  // called every frame at 60 frames per second
+  // handle input and update the game state
+
+  if (mouseIsDown(0)) {
+    const direction = mousePos.subtract(player.pos).normalize();
+    const position = player.pos.add(direction.scale(0.5));
+    const rateOfFire = 0.2; // configurable rate of fire
+    const currentTime = performance.now();
+    if (currentTime - lastFireTime > rateOfFire * 1000) {
+      new Projectile(position, direction);
+      lastFireTime = currentTime;
+    }
+  }
+}
+
+function gameUpdatePost() {
+  // called after physics and objects are updated
+  // setup camera and prepare for render
+}
+
 function gameRender() {
-  // draw a grey square in the background without using webgl
-  LittleJS.drawRect(vec2(16, 8), vec2(20, 14), hsl(0, 0, 0.6), 0, false);
-
-  // draw the logo as a tile
-  LittleJS.drawTile(vec2(21, 5), vec2(4.5), tile(3, 128));
+  // called before objects are rendered
+  // draw any background effects that appear behind objects
 }
 
-///////////////////////////////////////////////////////////////////////////////
 function gameRenderPost() {
-  // draw to overlay canvas for hud rendering
-  LittleJS.drawTextScreen(
-    "LittleJS with TypeScript",
-    vec2(LittleJS.mainCanvasSize.x / 2, 80),
-    80
-  );
+  // called after objects are rendered
+  // draw effects or hud that appear above all objects
 }
 
-///////////////////////////////////////////////////////////////////////////////
 // Startup LittleJS Engine
-LittleJS.engineInit(
-  gameInit,
-  gameUpdate,
-  gameUpdatePost,
-  gameRender,
-  gameRenderPost,
-  ["./assets/img/tiles.png"]
-);
+engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost, [
+  "./assets/img/player.png",
+]);
